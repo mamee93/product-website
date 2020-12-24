8c0569511615878e5c40a7817a4179dbf3c4d16e
maram <mameeal@gmail.com>
2020-12-24 16:46:30 +0400

    add product , order , Cart Page and Checkout Page

--- store/models.py
+++ store/models.py
@@ -17,10 +17,19 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
+    description = models.TextField(max_length=1000, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(upload_to='image_product',null=True, blank=True)
     def __str__(self):
         return self.name
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE, blank=True,null=True)
@@ -30,16 +39,31 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
-	# description = models.TextField(max_length=40000)
+    
+    def get_cart_item(self):
+        orderitem = self.orderitem_set.all()
+        total = sum([items.quantity for items in orderitem])
+        return total
+    
+    def get_cart_total(self):
+        orderitem = self.orderitem_set.all()
+        total = sum([items.get_total for items in orderitem])
+        return total
+	
 	# location = models.CharField(max_length=100)
 	# image = models.ImageField(upload_to='property/')
 	# category = models.ForeignKey('Category', related_name='room_category',on_delete=models.CASCADE, blank=True,null=True)
 	# slug    = models.SlugField(blank=True,null=True)  
 class OrderItem(models.Model):
-    Product = models.ForeignKey(Product,on_delete=models.CASCADE, blank=True,null=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, blank=True,null=True)
     order = models.ForeignKey(Order,on_delete=models.CASCADE, blank=True,null=True)
-    qunatity = models.IntegerField(default=0,null=True, blank=True)
+    quantity = models.IntegerField(default=0,null=True, blank=True)
     date_added = models.DateTimeField( auto_now_add=True)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
 
 class ShippingAddres(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE, blank=True,null=True)
--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "store"
 urlpatterns = [
     path('', views.store, name='store'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
 ]
